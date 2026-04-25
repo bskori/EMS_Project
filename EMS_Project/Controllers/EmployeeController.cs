5c6aef97dc61b6c0d34a2a339e5e023db3d2c502
Bipin <bskori9175@gmail.com>
2026-04-25 16:27:53 +0530

    Added Edit and Delete funtionality

--- EMS_Project/Controllers/EmployeeController.cs
+++ EMS_Project/Controllers/EmployeeController.cs
@@ -35,5 +35,42 @@ namespace EMS_Project.Controllers
             ViewBag.DeptId = new SelectList(db.Departments, "DeptId", "DeptName");
             return View(emp);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var emp = db.Employees.Find(id);
+            ViewBag.DeptId = new SelectList(db.Departments, "DeptId", "DeptName", emp.DeptId);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(emp).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DeptId = new SelectList(db.Departments, "DeptId", "DeptName", emp.DeptId);
+            return View(emp);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var emp = db.Employees.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var emp = db.Employees.Find(id);
+            db.Employees.Remove(emp);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
