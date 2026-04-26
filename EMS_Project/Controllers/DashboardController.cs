6a61dfc14b19e025bc1cb49e26ca64d98e643ac2
Bipin <bskori9175@gmail.com>
2026-04-26 10:13:44 +0530

    Added dashboard feature

---
+++ EMS_Project/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using EMS_Project.Models;
+using EMS_Project.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EMS_Project.Controllers
+{
+    public class DashboardController : Controller
+    {
+        EMS_DBEntities db = new EMS_DBEntities();
+        public ActionResult Index()
+        {
+            ViewBag.TotalEmployees = db.Employees.Count();
+            ViewBag.TotalDepartments = db.Departments.Count();
+            ViewBag.HighestSalary = db.Salaries.Max(s => s.GrossSalary);
+
+            var deptData = db.Departments.Select(d => new
+            DeptEmployeeCountVM
+            {
+                DeptName = d.DeptName,
+                Count = d.Employees.Count()
+            }).ToList();
+
+
+            return View(deptData);
+
+        }
+    }
+}
---
+++ EMS_Project/ViewModels/DeptEmployeeCountVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMS_Project.ViewModels
+{
+    public class DeptEmployeeCountVM
+    {
+        public string DeptName { get; set; }
+        public int Count { get; set; }
+    }
+}
